c5d93ef03b63f8cc3d191f84b301233ba7251e99
Victor Monroy <vmmonroyv@gmail.com>
2025-12-09 15:55:42 -0600

    Modificacion de ejemplo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <p style="text-align:center; margin-top:20px;">
-            Proyecto subido a GitHub — cambio de prueba.
+            Proyecto subido a GitHub — cambio de prueba. 15:54
         </p>
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
